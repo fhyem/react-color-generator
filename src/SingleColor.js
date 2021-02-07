ed493c20e022173717c75820e7a3bc4e699ec263
Fahim Ud Din <fhyembercha@gmail.com>
2021-02-08 02:39:14 +0500

    added Color generator app

---
+++ src/SingleColor.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = () => {
+  return <article className="color false">
+  <p className="percent-value">"100" "%"</p>
+  <p className="color-value">#ffffff</p>
+</article>
+}
+
+export default SingleColor
+;
